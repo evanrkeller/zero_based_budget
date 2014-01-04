3ecf864f36a85bda2add51c7f17f86697fd78516
Evan Keller <ekeller@barefootproximity.com>
2014-01-04 10:08:34 -0600

    Blank Rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZeroBasedBudget::Application.config.secret_key_base = 'a8b4b167a512ae0419127f4ceb72980520fff1352a2ad03031571e8314156814e7066c7e61ed1d6789eb478e133a57f50c300c3911d9d349f81d3b947954f77f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZeroBasedBudget::Application.config.session_store :cookie_store, key: '_zero_based_budget_session'
